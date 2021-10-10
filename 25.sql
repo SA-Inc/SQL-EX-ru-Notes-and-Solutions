0879a606bd0549e60201b41f3995097a2507d8af
Arman Saparov <a.saparov.dku@gmail.com>
2021-10-11 00:45:05 +0600

    Create 25.sql

---
+++ 25.sql
@@ -0,0 +1,22 @@
+SELECT DISTINCT pd.maker
+FROM Product AS pd
+JOIN PC AS pc ON pd.model = pc.model
+WHERE pc.ram = (SELECT MIN(ram) FROM pc)
+AND pc.code IN (
+  SELECT code 
+  FROM (
+    SELECT code, model, speed, ram 
+    FROM pc 
+    WHERE ram = (SELECT MIN(ram) FROM pc)
+  ) a 
+  WHERE speed = (
+    SELECT MAX(speed) 
+    FROM pc 
+    WHERE ram = (SELECT MIN(ram) FROM pc)
+  )
+)
+AND pd.maker IN (
+  SELECT maker 
+  FROM product 
+  WHERE type = 'printer'
+)
