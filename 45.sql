9c95c12ae965a511b85c9ad40afd001f99c5f035
Arman Saparov <a.saparov.dku@gmail.com>
2021-10-12 22:08:41 +0600

    Create 45.sql

---
+++ 45.sql
@@ -0,0 +1,12 @@
+-- Find all ship names consisting of three or more words (e.g., King George V).
+-- Consider the words in ship names to be separated by single spaces, and the ship names to have no leading or trailing spaces.
+
+SELECT name
+FROM Ships
+WHERE name LIKE '% % %'
+
+UNION
+
+SELECT ship
+FROM Outcomes
+WHERE ship LIKE '% % %'
